5a231df2328eb57c6cd1a94edce68bb6cd59f091
allinvestglobal <53827177+allinvestglobal@users.noreply.github.com>
2019-10-28 13:27:11 +0100

    Add files via upload

---
+++ amigo2.py
@@ -0,0 +1,1410 @@
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '1.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '2.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '3.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '4.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '5.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '6.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '7.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '8.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '9.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '10.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '11.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '12.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '13.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '14.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '15.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '16.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '17.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+import time
+
+time.sleep(1800)  # delays for 30 minutes
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '18.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '20.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '21.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '22.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '23.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '24.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '25.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '27.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '28.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '29.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '30.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '31.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '32.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '33.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '26.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '34.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+import time
+
+time.sleep(1800)  # delays for 30 minutes
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '35.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '36.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '37.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '38.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '39.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '40.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '41.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '42.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '43.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '44.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '45.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '46.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '47.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '48.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '49.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+import time
+
+time.sleep(1800)  # delays for 30 minutes
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '50.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '51.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '52.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '53.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '54.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '55.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '56.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '57.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '58.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '59.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '60.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '61.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '62.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '63.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '64.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '65.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '66.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+import time
+
+time.sleep(1800)  # delays for 30 minutes
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '67.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '68.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '69.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '70.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '71.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '72.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '73.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '74.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '75.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '76.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '77.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '78.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '79.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '80.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '81.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+import time
+
+time.sleep(1800)  # delays for 30 minutes
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '82.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '83.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '84.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '84.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '86.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '87.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '88.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '89.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
+
+
+from instapy_cli import client
+
+username = 'zaynedeespana'
+password = '529million'
+image = '90.jpg'
+text = ''
+
+with client(username, password) as cli:
+    cli.upload(image, text)
+
+
+
